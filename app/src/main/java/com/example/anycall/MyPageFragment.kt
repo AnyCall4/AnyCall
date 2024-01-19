ee46f8203fe4ceb7c58d8e1342d28eb6929790a3
kiwon kim <rlarldnjs17@gmail.com>
2024-01-19 10:54:14 +0900

    Merge: pull

--- app/src/main/java/com/example/anycall/MyPageFragment.kt
+++ app/src/main/java/com/example/anycall/MyPageFragment.kt
@@ -1,16 +1,13 @@
 package com.example.anycall
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.Fragment
 import com.example.anycall.databinding.FragmentMyPageBinding
 import com.example.anycall.databinding.MyPageDialogBinding
 
